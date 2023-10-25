05df91b82956e34ebdbbae694980eedd2ed0cbc8
shash-786 <ssalian60@yahoo.com>
2023-10-25 20:19:56 +0530

    Removed Primes Bug

--- user/primes.c
+++ user/primes.c
@@ -58,12 +58,13 @@ int main(int argc, char *argv[]) {
 	pipe(p);
 
 	for(int i=2; i<35; i++) {
-		write(p[1],&i,sizeof(int));
+		int n = i;
+		write(p[1],&n,sizeof(int));
 	}
 
 	close(p[1]);
 	exec_pipe(p[0]);
 	close(p[0]);
-
+	sleep(2);
 	return 0;
 }
