0ced7b59c5db80e9fbe31d438a60df8286221c53
shash-786 <ssalian60@yahoo.com>
2023-10-25 18:56:32 +0530

    pingpong added

---
+++ user/pingpong.c
@@ -0,0 +1,28 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char const *argv[])
+ {
+ 	int p[2];
+ 	pipe(p);
+ 	char buff[10];
+
+ 	int pid = fork();
+
+ 	if(pid == 0) {
+ 		close(p[0]);
+ 		write(p[1],"ping",4);
+ 		printf("%d: received ping\n",getpid());
+ 		close(p[1]);
+ 	}
+
+ 	else{
+ 		wait(0);
+ 		close(p[1]);
+ 		read(p[0],buff,4);
+ 		printf("%d: received pong\n",getpid());
+ 		close(p[0]);
+ 	}
+
+ 	return 0;
+ } 
